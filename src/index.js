aa8fbe9f0f41ad0eb7fd34de48fbf6dd95e0d644
Refayat Haque <refayathaque@gmail.com>
2019-02-24 19:37:49 -0500

    staged, committed and pushing to master

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// const App = function() {
+//   return <div>Hi there!</div>
+// };
+
+// Latest ES6 way of writing a function - "Arrow" function
+const App = () => {
+  return <div>Hi there!</div>
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
